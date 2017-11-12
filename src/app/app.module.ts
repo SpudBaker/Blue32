b825a2521bb64baee4d6a05e4b965ee17c117cb0
SpudBaker <spudbaker@gmail.com>
2017-11-12 17:22:12 +0000

    basic structure with routes and material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule, MatToolbarModule, MatButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { CompanyComponent } from './company/company.component';
+import { PageNotFoundComponent } from './pageNotFound/pageNotFound.component';
+import { HomeComponent } from './home/home.component';
 
+const appRoutes: Routes = [
+  { path: 'company', component: CompanyComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '',
+    redirectTo: '/company',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CompanyComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      // { enableTracing: true } // <-- debugging purposes only
+    ),
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
